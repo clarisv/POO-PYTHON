94918403b762a07cef0e1d91400130ef560c748c
Clarice Christine <claricesoarescv@gmail.com>
2025-04-28 21:18:30 -0300

    Create exemplo_class.py

---
+++ exemplo_class.py
@@ -0,0 +1,29 @@
+'''class Carro:
+   def __init__(self, modelo, cor, ano):
+      self.modelo = modelo
+      self.cor = cor
+      self.ano = ano
+
+carro1 = Carro(modelo = 'BMW', cor = 'azul marinho', ano = 2012)'''
+
+'''class Restaurante:
+    def __init__(self, nome, categoria, avaliacao, horario_funcionamento, ativo = False):
+        self.nome = nome
+        self.categoria = categoria
+        self.avaliacao = avaliacao
+        self.horario_funcionamento = horario_funcionamento
+        self.ativo = ativo
+
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+
+restaurante1 = Restaurante(nome = 'bitelo', categoria = 'italiana', avaliacao = '5 estrelas', horario_funcionamento = '9h as 22h')'''
+
+'''class Cliente:
+    def __init__(self, nome, cpf, telefone, gmail):
+        self.nome = nome
+        self.cpf = cpf
+        self.telefone = telefone
+        self.gmail = gmail
+
+cliente1 = Cliente(nome = 'alice', cpf = '52648502631', telefone = '61 985254565', gmail = 'alicekelly@gmail.com')'''
